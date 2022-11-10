5d1bd2fdc17246484a374030e03e387efca461f6
Jeeva Prakash <jeevarag1016@gmail.com>
2022-11-10 18:13:03 +0530

    commit on 06:00PM 09/11/2022

--- app/src/main/java/com/c1ph3r/zomatorestaurant/RegisterRestaurant.java
+++ app/src/main/java/com/c1ph3r/zomatorestaurant/RegisterRestaurant.java
@@ -2,17 +2,21 @@ package com.c1ph3r.zomatorestaurant;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.c1ph3r.zomatorestaurant.Adapter.RegistrationProcess;
 import com.c1ph3r.zomatorestaurant.databinding.ActivityRegisterRestaurantBinding;
-import com.google.android.material.button.MaterialButton;
+import com.c1ph3r.zomatorestaurant.databinding.FragmentRegisterPage1Binding;
 
 public class RegisterRestaurant extends AppCompatActivity {
 
     private ActivityRegisterRestaurantBinding NEW_RESTAURANT;
-
+    private FragmentRegisterPage1Binding PAGE1;
+    RegisterPage1 restaurantDetails;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +24,11 @@ public class RegisterRestaurant extends AppCompatActivity {
         NEW_RESTAURANT = ActivityRegisterRestaurantBinding.inflate(getLayoutInflater());
         View view = NEW_RESTAURANT.getRoot();
         setContentView(view);
+        restaurantDetails = new RegisterPage1();
 
 
         RegistrationProcess process = new RegistrationProcess(RegisterRestaurant.this);
-        process.addFragment(new RegisterPage1());
+        process.addFragment(restaurantDetails);
         process.addFragment(new RegistrationPage2());
 
         NEW_RESTAURANT.RegistrationProcess.setAdapter(process);
@@ -33,10 +38,40 @@ public class RegisterRestaurant extends AppCompatActivity {
         NEW_RESTAURANT.NextButton.setOnClickListener( OnClickNext -> {
             switch (NEW_RESTAURANT.RegistrationProcess.getCurrentItem()){
                 case 0:
-                    NEW_RESTAURANT.RegistrationProcess.setCurrentItem(1);
-                    NEW_RESTAURANT.NextButton.setText("Submit");
+                    checkInputs();
                     break;
             }
         });
+    }
+
+    private void checkInputs() {
+        View view = restaurantDetails.getView();
+        if(view != null){
+            PAGE1 = FragmentRegisterPage1Binding.bind(view);
+            if(PAGE1.OwnerName.getText().toString().matches( "^[A-Za-z]\\w{5,28}$")){
+                System.out.println(PAGE1.OwnerName.getText().toString());
+            }else
+               alertTheUser("Enter a valid UserName!", "UserName must starts with alphabets(Aa-Zz) and not more than 28 words.")
+                       .show();
+        }
+    }
+
+
+    Dialog alertTheUser(String Title, String Message){
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.custom_dialog);
+        TextView TITLE, MESSAGE;
+        Button Ok;
+        TITLE = dialog.findViewById(R.id.TITLE);
+        MESSAGE = dialog.findViewById(R.id.MESSAGE);
+        Ok = dialog.findViewById(R.id.Ok);
+
+        TITLE.setText(Title);
+        MESSAGE.setText(Message);
+        Ok.setOnClickListener(onClickOk -> dialog.cancel());
+        dialog.create();
+
+        return dialog;
+
     }
 }
--- app/src/main/java/com/c1ph3r/zomatorestaurant/RegistrationPage2.java
+++ app/src/main/java/com/c1ph3r/zomatorestaurant/RegistrationPage2.java
@@ -1,18 +1,26 @@
 package com.c1ph3r.zomatorestaurant;
 
-import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
-import androidx.core.app.ActivityCompat;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
+import android.widget.ImageView;
+
+import com.google.android.material.button.MaterialButton;
 
 public class RegistrationPage2 extends Fragment {
-
+    ImageView previewDocument;
+    ActivityResultLauncher<Intent> openGallery;
 
     public RegistrationPage2() {
         // Required empty public constructor
@@ -26,12 +34,35 @@ public class RegistrationPage2 extends Fragment {
         View view = inflater.inflate(R.layout.fragment_registration_page2, container, false);
 
         if(view != null){
-            shouldShowRequestPermissionRationale(Manifest.permission_group.STORAGE);
-            if(ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), Manifest.permission_group.STORAGE)){
-                Toast.makeText(requireActivity(), "Permission Granted", Toast.LENGTH_SHORT).show();
-            }
+            MaterialButton chooseFile = view.findViewById(R.id.uploadDocument);
+            chooseFile.setOnClickListener(onClickChooseFile -> {
+                openGallery();
+            });
+            previewDocument = view.findViewById(R.id.PreviewDocument);
+            openSomeActivityForResult();
+
         }
 
         return view;
+    }
+
+    private void openGallery() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        openGallery.launch(Intent.createChooser(intent,"Select Document"));
+    }
+
+    public void openSomeActivityForResult(){
+        openGallery = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if(result.getResultCode() == Activity.RESULT_OK){
+                        Intent data = result.getData();
+                        assert data != null;
+                        Uri imageUri = data.getData();
+                        previewDocument.setImageURI(imageUri);
+                    }
+                });
     }
 }
